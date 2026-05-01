d27c8f1d0597ae5035d904467c7e911d14aa5b77
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2026-05-01 00:52:37 -0300

    Commit autom├ítico via BAT em 01/05/2026 ├ás  0:52:37,22

---
+++ PUSH arquivos no Github.bat
@@ -0,0 +1,19 @@
+@echo off
+cd /d "%~dp0"
+
+:: Mensagem de commit automática com data e hora
+set COMMIT_MSG=Commit automático via BAT em %date% às %time%
+
+echo Adicionando todos os arquivos modificados...
+git add .
+
+echo Fazendo commit com a mensagem: %COMMIT_MSG%
+git commit -m "%COMMIT_MSG%"
+
+echo Enviando para o repositorio remoto...
+git push
+
+echo Operação concluída.
+pause
+
+
